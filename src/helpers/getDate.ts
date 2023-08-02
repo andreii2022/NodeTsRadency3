4899d75d852d4156fa92232fc8e4f7fc29e95da1
Andrii Nikulin <nikulin.and90@gmail.com>
2023-08-02 12:50:36 +0300

    fix component

---
+++ src/helpers/getDate.ts
@@ -0,0 +1,12 @@
+const getDate = () => {
+    const date = new Date();
+    const dateOfCreation = date.toLocaleDateString("en-US", {
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+    });
+    return dateOfCreation;
+  };
+  
+  export default getDate;
+  
