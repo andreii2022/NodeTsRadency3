8f021ca9f7812e0c6824f6357f0efc8efbdbbde9
Andrii Nikulin <nikulin.and90@gmail.com>
2023-08-06 12:01:29 +0300

    Updating records in the database and deleting records from the database

--- app.js
+++ app.js
@@ -21,3 +21,35 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Сервер запущено на порту ${PORT}`);
 });
+
+app.post('/notes', async (req, res) => {
+  try {
+    const { title, content } = req.body;
+    const newNote = await Note.create({ title, content });
+    res.status(201).json(newNote);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Помилка сервера' });
+  }
+});
+app.put('/notes/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, content } = req.body;
+    const updatedNote = await Note.update({ title, content }, { where: { id } });
+    res.json(updatedNote);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Помилка сервера' });
+  }
+});
+app.delete('/notes/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Note.destroy({ where: { id } });
+    res.status(204).send();
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Помилка сервера' });
+  }
+});
